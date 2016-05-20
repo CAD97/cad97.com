949c4d71e4c17a109a42c7b9303490dfdc4a28a8
Christopher Durham <Christopher Durham>
2016-05-20 03:16:59 -0400

    Remove dev build
    I couldn't get the sourcemaps working anyway.
    
    Maybe another time.

--- gulpfile.ts
+++ gulpfile.ts
@@ -9,7 +9,6 @@ var browserify = require("browserify");
 var source = require("vinyl-source-stream");
 var tsify = require("tsify");
 var uglify = require("gulp-uglify");
-var sourcemaps = require("gulp-sourcemaps");
 var buffer = require("vinyl-buffer");
 
 @Gulpclass(gulp)
@@ -43,28 +42,7 @@ export class Gulpfile {
           .pipe(source("bundle.js"))
           .pipe(buffer())
           .pipe(uglify())
-          .pipe(sourcemaps.write("./"))
           .pipe(gulp.dest("www"));
     }
-    
-    @Task("dev-build", ["copy-files"])
-    dev() {
-        this.copyFiles();
-        return browserify({
-            basedir: '.',
-            debug: true,
-            entries: ['src/main.ts'],
-            cache: {},
-            packageCache: {}
-        }).plugin(tsify)
-          .bundle()
-          .pipe(source("bundle.js"))
-          .pipe(buffer())
-          .pipe(sourcemaps.init({loadMaps: true}))
-          .pipe(uglify())
-          .pipe(sourcemaps.write("./"))
-          .pipe(gulp.dest("www"));
-    }
-    
 }
 
