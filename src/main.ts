85a412ce2c3d75da4cda3e37c02856749ab35a24
Christopher Durham <Christopher Durham>
2016-05-20 01:21:18 -0400

    Basic layout of website done!
    Ofc I've got to fill in all of the content and do the more interesting things but this is nice I've got the header/drawer/footer set up and reusable from one spot

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,62 @@
 
-import { sayHello } from "./greet";
+/// <reference path="../typings/index.d.ts" />
 
-function showHello(divName: string, name: string) {
-    const elt = document.getElementById(divName);
-    elt.innerText = sayHello(name);
+function makeHeader(el: HTMLElement) {
+    el.className = "mdl-layout__header mdl-shadow--8dp";
+    el.innerHTML =
+    `   <div class="mdl-layout-icon"></div>
+        <div class="mdl-layout__header-row">
+            <span class="mdl-layout__title">CAD97</span>
+            <div class="mdl-layout-spacer"></div>
+            <nav></nav>
+        </div>
+    `;
+    makeNavigation((el.children[1] as HTMLElement).children[2] as HTMLElement);
+    componentHandler.upgradeElement(el);
 }
 
-showHello("greeting", "TypeScript");
+function makeDrawer(el: HTMLElement) {
+    el.className = "mdl-layout__drawer";
+    el.innerHTML =
+    `   <span class="mdl-layout__title">CAD97</span>
+        <nav></nav>
+    `;
+    makeNavigation(el.children[1] as HTMLElement);
+    componentHandler.upgradeElement(el);
+}
+
+function makeNavigation(el: HTMLElement) {
+    el.className = "mdl-navigation";
+    el.innerHTML = 
+    `   <a class="mdl-navigation__link" href="#">Nav link 1</a>
+        <a class="mdl-navigation__link" href="#">Nav link 2</a>
+        <a class="mdl-navigation__link" href="#">Nav link 3</a>
+    `;
+    componentHandler.upgradeElement(el);
+}
+
+function makeFooter(el: HTMLElement) {
+    el.className = "mdl-mini-footer";
+    el.innerHTML =
+    `
+        <div class="mdl-mini-footer__left-section">
+            <div class="mdl-logo">
+                Mini-footer Heading
+            </div>
+            <ul class="mdl-mini-footer__link-list">
+                <li><a href="">Link 1</a></li>
+                <li><a href="">Link 2</a></li>
+                <li><a href="">Link 3</a></li>
+            </ul>
+        </div>
+        <div class="mdl-mini-footer__right-section">
+            <button class="mdl-mini-footer__social-btn"></button>
+            <button class="mdl-mini-footer__social-btn"></button>
+            <button class="mdl-mini-footer__social-btn"></button>
+        </div>
+    `;
+}
+
+makeHeader(document.getElementById("header"));
+makeDrawer(document.getElementById("drawer"));
+makeFooter(document.getElementById("footer"));
