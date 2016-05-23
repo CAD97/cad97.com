d64b15a08ec9c5c0ce0a85fd4b6ad864fd2bc7d6
Christopher Durham <Christopher Durham>
2016-05-22 21:24:31 -0400

    Set up card architecture
    Look at me, I'm writing dirty .js code!

--- src/main.ts
+++ src/main.ts
@@ -71,3 +71,21 @@ function makeFooter(el: HTMLElement) {
 makeHeader(document.getElementById("header"));
 makeDrawer(document.getElementById("drawer"));
 makeFooter(document.getElementById("footer"));
+
+function makeCellCard(el: HTMLElement, width: number, shadow?: number) {
+    el.classList.remove(`cell-${width}`);
+    el.classList.add("mdl-cell", `mdl-cell--${width}-col`);
+    if (shadow) el.classList.add(`mdl-shadow--${shadow}dp`);
+    
+    var title = el.children[0] as HTMLElement;
+    var body = el.children[1] as HTMLElement;
+    
+    title.classList.add("mdl-card__title");
+    title.children[0].classList.add("mdl-card__title-text");
+    body.classList.add("mdl-card__supporting-text");
+}
+
+[1,2,3,4,5,6,7,8,9,10,11,12].forEach(i=>
+    Array.prototype.forEach.call(document.getElementsByClassName(`cell-${i}`),
+                                 (el:HTMLElement) => makeCellCard(el, i, 4))
+);
