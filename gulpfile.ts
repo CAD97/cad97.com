9e8d82a5be50c28eb021743beffbcc1e09791822
Christopher Durham <cad97@cad97.com>
2016-06-07 22:30:14 -0400

    tslint

--- gulpfile.ts
+++ gulpfile.ts
@@ -8,42 +8,42 @@ import * as sourcemaps from "gulp-sourcemaps";
 import * as uglify from "gulp-uglify";
 import * as del from "del";
 
-const tsProject = ts.createProject('tsconfig.json');
+const tsProject = ts.createProject("tsconfig.json");
 
-var paths = {
-    ts: ['src/main.ts'],
-    html: 'src/**/*.html',
-    css: 'src/**/*.css',
-    img: ['src/**/*.ico', 'src/**/*.png'],
-    dest: 'www'
+const paths = {
+    ts: ["src/main.ts"],
+    html: "src/**/*.html",
+    css: "src/**/*.css",
+    img: ["src/**/*.ico", "src/**/*.png"],
+    dest: "www"
 }
 
 @Gulpclass(gulp)
 export class Gulpfile {
-    
+
     @Task("clean")
     clean(cb: Function) {
-        return del([paths.dest + '/**'], cb);
+        return del([paths.dest + "/**"], cb);
     }
-    
+
     @Task("copy-html")
     copyHTML() {
         return gulp.src(paths.html)
                    .pipe(gulp.dest(paths.dest));
     }
-    
+
     @Task("copy-css")
     copyCSS() {
         return gulp.src(paths.css)
                    .pipe(gulp.dest(paths.dest));
     }
-    
+
     @Task("copy-img")
     copyICO() {
         return gulp.src(paths.img)
                    .pipe(gulp.dest(paths.dest));
     }
-    
+
     @Task("transpile")
     transpile() {
         return gulp.src(paths.ts)
@@ -53,33 +53,33 @@ export class Gulpfile {
                    .pipe(sourcemaps.write("./"))
                    .pipe(gulp.dest(paths.dest));
     }
-    
+
     @Task("watch")
     watch() {
-        gulp.watch(paths.ts, ['transpile']);
-        gulp.watch(paths.html, ['copy-html']);
-        gulp.watch(paths.css, ['copy-css']);
-        gulp.watch(paths.img, ['copy-img']);
+        gulp.watch(paths.ts, ["transpile"]);
+        gulp.watch(paths.html, ["copy-html"]);
+        gulp.watch(paths.css, ["copy-css"]);
+        gulp.watch(paths.img, ["copy-img"]);
     }
-    
+
     @SequenceTask("copy-static")
     copyFiles() {
-        return [['copy-html', 'copy-css', 'copy-img']];
+        return [["copy-html", "copy-css", "copy-img"]];
     }
-    
+
     @SequenceTask("build")
     build() {
         return [["copy-static", "transpile"]];
     }
-    
+
     @SequenceTask("clean-build")
     cleanBuild() {
         return ["clean", "build"];
     }
-    
+
     @SequenceTask("start-dev")
     startDev() {
-        return ['clean-build', 'watch'];
+        return ["clean-build", "watch"];
     }
 }
 
--- src/main.ts
+++ src/main.ts
@@ -27,7 +27,7 @@ function makeDrawer(el: HTMLElement) {
 
 function makeNavigation(el: HTMLElement) {
     el.classList.add("mdl-navigation");
-    el.innerHTML = 
+    el.innerHTML =
     `   <a class="mdl-navigation__link" href="/">Home</a>
         <a class="mdl-navigation__link" href="#">Anden's Mountain</a>
         <a class="mdl-navigation__link" href="#">The Tower of Babble</a>
@@ -41,7 +41,7 @@ function makeFooter(el: HTMLElement) {
     const spacer = document.createElement("div");
     spacer.classList.add("mdl-layout-spacer");
     par.insertBefore(spacer, el);
-    
+
     el.classList.add("mdl-mini-footer");
     el.innerHTML =
     `   <div class="mdl-mini-footer__left-section">
@@ -81,19 +81,19 @@ function makeCellCard(el: HTMLElement, width: number, shadow?: number) {
     el.classList.remove(`cell-${width}`);
     el.classList.add("mdl-cell", "mdl-card", `mdl-cell--${width}-col`, "mdl-cell--8-col-tablet");
     if (shadow) el.classList.add(`mdl-shadow--${shadow}dp`);
-    
+
     const title = el.children[0] as HTMLElement;
     const body = el.children[1] as HTMLElement;
-    
+
     title.classList.add("mdl-card__title");
     title.children[0].classList.add("mdl-card__title-text");
     if (title.childElementCount > 1) title.children[1].classList.add("mdl-layout-spacer")
     body.classList.add("mdl-card__supporting-text");
-    
+
     componentHandler.upgradeElement(el);
 }
 
-[1,2,3,4,5,6,7,8,9,10,11,12].forEach(i => {
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].forEach(i => {
     const list = document.getElementsByClassName(`cell-${i}`);
     while (list.length) makeCellCard(list[0] as HTMLElement, i, 4);
 });
