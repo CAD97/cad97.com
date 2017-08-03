c32bed088805f5bd68d9e84112209f791c0b0145
CAD97 <cad97@cad97.com>
2017-08-03 16:26:31 -0400

    Populate view and partials from same folder.
    This means I can use any of the partial snippets via {{include}} or {{> recursive_include}}.

--- gulpfile.ts
+++ gulpfile.ts
@@ -3,22 +3,14 @@ import * as ts from "gulp-typescript";
 import * as del from "del";
 import * as mustache from "gulp-mustache";
 import * as fs from "fs";
+import * as memoize from "memoizee";
 
 const tsProject = ts.createProject("tsconfig.json");
 const paths = {
     ts: "src/js/**/*.ts",
     mustache: {
         sources: "src/html/**/*.mustache",
-        view: () => {
-            let view: { [id: string]: string } = {};
-            let files = fs.readdirSync("src/templates/");
-            files.forEach((file) => {
-                view[file.replace(/\.[^/.]+$/, "")] =
-                    fs.readFileSync(`src/templates/${file}`).toString();
-            });
-            return view;
-        },
-        partials: () => {
+        partials: memoize(() => {
             let view: { [id: string]: string } = {};
             let files = fs.readdirSync("src/partials/");
             files.forEach((file) => {
@@ -26,7 +18,7 @@ const paths = {
                     fs.readFileSync(`src/partials/${file}`).toString();
             });
             return view;
-        }
+        })
     },
     www: "www"
 };
@@ -37,7 +29,7 @@ gulp.task("clean", () => {
 
 gulp.task("serve-html", () => {
     return gulp.src(paths.mustache.sources)
-        .pipe(mustache(paths.mustache.view(), { extension: ".html" }, paths.mustache.partials()))
+        .pipe(mustache(paths.mustache.partials(), { extension: ".html" }, paths.mustache.partials()))
         .pipe(gulp.dest(paths.www));
 });
 
