5ac0c8b54b9d84cc2139f4b069edc9cfa7d5b193
CAD97 <cad97@cad97.com>
2017-08-03 21:26:10 -0400

    Browsersync for easy testing!

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,24 +1,29 @@
+import * as browsersync from "browser-sync";
 import * as del from "del";
 import * as fs from "fs";
 import * as gulp from "gulp";
 import * as less from "gulp-less";
 import * as mustache from "gulp-mustache";
-import * as ts from "gulp-typescript";
+import * as typescript from "gulp-typescript";
 import * as memoize from "memoizee";
 
-const tsProject = ts.createProject("tsconfig.json");
+const browser = browsersync.create();
+const tsProject = typescript.createProject("tsconfig.json");
 const paths = {
     less: ["src/css/**/*.less"],
     mustache: {
-        partials: memoize(() => {
-            let view: { [id: string]: string } = {};
-            let files = fs.readdirSync("src/partials/");
-            files.forEach((file) => {
-                view[file.replace(/\.[^/.]+$/, "")] =
-                    fs.readFileSync(`src/partials/${file}`).toString();
-            });
-            return view;
-        }),
+        partials: {
+            dir: "src/partials/",
+            view: memoize(() => {
+                let view: { [id: string]: string } = {};
+                let files = fs.readdirSync(paths.mustache.partials.dir);
+                files.forEach((file) => {
+                    view[file.replace(/\.[^/.]+$/, "")] =
+                        fs.readFileSync(`src/partials/${file}`).toString();
+                });
+                return view;
+            }),
+        },
         sources: ["src/html/**/*.mustache"],
     },
     ts: ["src/js/**/*.ts"],
@@ -34,20 +39,35 @@ gulp.task("serve-css", () => {
         .pipe(less({
             paths: paths.less,
         }))
-        .pipe(gulp.dest(paths.www));
+        .pipe(gulp.dest(paths.www))
+        .pipe(browser.stream());
 });
 
 gulp.task("serve-html", () => {
     return gulp.src(paths.mustache.sources)
-        .pipe(mustache(paths.mustache.partials(), {extension: ".html"}, paths.mustache.partials()))
-        .pipe(gulp.dest(paths.www));
+        .pipe(mustache(paths.mustache.partials.view(), {extension: ".html"}, paths.mustache.partials.view()))
+        .pipe(gulp.dest(paths.www))
+        .pipe(browser.stream());
 });
 
 gulp.task("serve-js", () => {
     return gulp.src(paths.ts)
         .pipe(tsProject())
-        .pipe(gulp.dest(paths.www));
+        .pipe(gulp.dest(paths.www))
+        .pipe(browser.stream());
 });
 
 gulp.task("build", gulp.parallel("serve-css", "serve-html", "serve-js"));
-gulp.task("default", gulp.series("clean", "build"));
+gulp.task("default", gulp.series("clean", "build", function browserify() {
+    browser.init({
+        https: true,
+        server: {
+            baseDir: paths.www,
+        },
+    });
+
+    gulp.watch(paths.less, gulp.parallel("serve-css"));
+    gulp.watch(paths.mustache.sources, gulp.parallel("serve-html"));
+    gulp.watch(paths.mustache.partials.dir, gulp.parallel("serve-html"));
+    gulp.watch(paths.ts, gulp.parallel("serve-js"));
+}));
